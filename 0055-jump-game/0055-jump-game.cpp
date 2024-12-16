0700477fbb69ee422ee8bb84ba222cb5fac5963a
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2024-12-15 20:10:36 -0600

    0055-JUMP-GAME Stats: Time: 0 ms (100%), Space: 52.4 MB (18.61%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxJump = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(i > maxJump){
+                return false;
+            }
+
+            maxJump = max(maxJump, nums[i] + i);
+            if(maxJump >= nums.size() - 1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
