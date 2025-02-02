8fb4e5d939780bcb253d70c05486d5f85adbb410
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2025-02-01 20:05:29 -0600

    0169-MAJORITY-ELEMENT Stats: Time: 0 ms (100%), Space: 28.3 MB (24.67%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,14 +1,9 @@
-#include <unordered_map>
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         unordered_map <int, int> numVal;
         for(int i = 0; i < nums.size(); i++){
-            if(numVal.find(nums[i]) == numVal.end()){
-                numVal[nums[i]] = 1;
-            }else{
-                numVal[nums[i]]++;
-            }
+            numVal[nums[i]]++;
         }
         pair<int, int> biggest = {-1, -1};
         for(auto i : numVal){
