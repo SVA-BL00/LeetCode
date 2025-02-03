d7e05c6d6356d08f2270a2141f6c6f94cfa1c5c7
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2025-02-02 19:55:43 -0600

    3372-LONGEST-STRICTLY-INCREASING-OR-STRICTLY-DECREASING-SUBARRAY Stats: Time: 0 ms (100%), Space: 28 MB (7.06%) - LeetHub

---
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        if(nums.size() == 1) return 1;
+        int longest = 0, dec = 0, inc = 0;
+        for(int i = 0; i < nums.size() - 1; i++){
+            if(nums[i+1] > nums[i]){
+                longest = max(longest, dec);
+                dec = 0;
+                if(inc == 0){
+                    inc += 2;
+                }else{
+                    inc++;
+                }
+            }else if(nums[i+1] < nums[i]){
+                longest = max(longest, inc);
+                inc = 0;
+                if(dec == 0){
+                    dec += 2;
+                }else{
+                    dec++;
+                }
+            }else{
+                longest = max(longest, inc);
+                longest = max(longest, dec);
+                dec = 0, inc = 0;
+                longest = max(longest, 1);
+            }
+        }
+        
+        longest = max(longest, inc);
+        longest = max(longest, dec);
+        return longest;
+    }
+};
