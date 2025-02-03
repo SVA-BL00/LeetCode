de4c3d46681c3fe059edb00a29ed910d08036748
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2025-02-02 18:42:51 -0600

    0200-NUMBER-OF-ISLANDS Stats: Time: 18 ms (97.63%), Space: 16 MB (96.06%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    void findIsland(vector<vector<char>>& grid, vector<vector<bool>>& seen, int i, int j){
-        if(grid[i][j] == '0' || seen[i][j] == true) return;
-        seen[i][j] = true;
-        if(i+1 < grid.size()) findIsland(grid, seen, i+1, j);
-        if(j+1 < grid[0].size()) findIsland(grid,seen, i, j+1);
-        if(i-1 >= 0) findIsland(grid, seen, i-1, j);
-        if(j-1 >= 0) findIsland(grid, seen, i, j-1);
+    // First submission used a second matrix to check for seen parts of the island.
+    // Memory could be O(1) if the chars are changed directly to 0.
+    // My second attempt:
+    void findIsland(vector<vector<char>>& grid, int i, int j){
+        if(grid[i][j] == '0') return;
+        grid[i][j] = '0';
+        if(i+1 < grid.size()) findIsland(grid, i+1, j);
+        if(j+1 < grid[0].size()) findIsland(grid, i, j+1);
+        if(i-1 >= 0) findIsland(grid, i-1, j);
+        if(j-1 >= 0) findIsland(grid, i, j-1);
     }
 
     int numIslands(vector<vector<char>>& grid) {
-        vector<vector<bool>> seen(grid.size(), vector<bool>(grid[0].size(), false));
         int res = 0;
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < grid[0].size(); j++){
-                if(seen[i][j] || grid[i][j] == '0'){
+                if(grid[i][j] == '0'){
                     continue;
                 }else{
-                    findIsland(grid, seen, i, j);
+                    findIsland(grid, i, j);
                     res++;
                 }
             }
