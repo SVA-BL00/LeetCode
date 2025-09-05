540c1fde50c9675bd76254a6c97ea1458b9a54ca
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2025-09-05 15:39:15 -0400

    0242-VALID-ANAGRAM Stats: Time: 25 ms (34.62%), Space: 13.2 MB (52.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,20 +5,5 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        map_string = {}
-        if len(s) != len(t): return False
-        for i in range(len(s)):
-            if s[i] in map_string:
-                map_string[s[i]] = map_string[s[i]] + 1
-            else:
-                map_string[s[i]] = 1
-
-        for j in range(len(t)):
-            if t[j] in map_string and map_string[t[j]] > 0:
-                map_string[t[j]] = map_string[t[j]] - 1
-            else:
-                return False
-        
-        print len(map_string)
-        return True
-        
+        if "".join(sorted(s)) == "".join(sorted(t)): return True
+        return False
